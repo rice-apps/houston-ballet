a50515c46c16e84b8483d6090454c4685117f689
Yimo Wang <yw104@rice.edu>
2023-10-28 14:50:25 -0500

    retrieved phone number from input, writing to csv

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 //
 'use client'
-
 import styled from "styled-components";
 import Image from 'next/image'
 import * as React from 'react';
+import * as Papa from 'papaparse';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Modal from '@mui/material/Modal';
-import InputText from 'inputtext' 
+import {parse} from 'papaparse';
+// import {readFileSync, writeFileSync} from 'fs';
+// import InputText from './inputtext' 
+
+const fs = require('fs');
 
 const modalStyle = {
   position: 'absolute' as 'absolute',
@@ -36,11 +40,34 @@ export function BasicModal() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const [phoneNumber, setPhoneNumber] = React.useState('');
-  // const handleSubmit = (e: string) => setPhoneNumber(e);
-   // var phoneNumber:string;
-  // const handleSubmit = (e) => 
 
+  const [phoneNumber, setPhoneNumber] = React.useState('');
+  const [csvContents, setCsvContents] = React.useState('');
+
+  // Store phone number from input textbox.
+  const handleSubmit = () => {
+    const filePath = 'phonenumbers.csv';
+    if (phoneNumber != '') {
+      console.log("here");
+      console.log(phoneNumber)
+
+      const newNumber = phoneNumber;
+      
+      // Open csv file and write to csvContents.
+      const phoneNumberFile = fs.readFileSync(filePath, 'utf8');
+      var allNumbers = Papa.parse<string>(phoneNumberFile).data;
+      
+      // Modify allNumbers.
+      allNumbers.push(newNumber)
+
+      // Save allNumbers to csv file.
+      setCsvContents(Papa.unparse(allNumbers));
+      fs.writeFileSync(filePath, csvContents, 'utf8')      
+    }
+    // TODO: error handling for invalid phone numbers
+    
+
+  }
   
   
   return (
@@ -59,12 +86,10 @@ export function BasicModal() {
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             explain why we need their information
           </Typography>
-          <Box>
-            {/* <TextField id="standard-basic" label="Name" variant="standard" /> */}
-            <TextField id="standard-basic" label="Phone Number" variant="standard" />
-            {/* <label><input type="text" placeholder="Phone Number" required></input></label> */}
-            {/* <Button onClick={handleSubmit}>Submit</Button> */}
-            <InputText onSave={(text) => setPhoneNumber(text)}></InputText>
+          <Box> 
+            {/* Textbox for phone number */}
+            <TextField id="phone_number" label="Phone Number" variant="standard" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+            <Button onClick={handleSubmit}>submit</Button>
           </Box>
         </Box>
       </Modal>
@@ -74,11 +99,13 @@ export function BasicModal() {
 
 
 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h2>"test"</h2>
+        {/* User information input box */}
         <BasicModal />
       </div>
     </main>
