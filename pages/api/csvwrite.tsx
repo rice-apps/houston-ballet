e394c6aa33d61451033031630cd95ebbfa5437bf
Keshav Shah <kks8@rice.edu>
2023-11-04 14:25:44 -0500

    added api endpoint in order to send phone number to server side and tested with postman

--- pages/api/csvwrite.tsx
+++ pages/api/csvwrite.tsx
@@ -5,13 +5,14 @@ type ResponseData = {
   message: string
 }
  
-export default function writeNumber(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ message: 'Hello from Next.js!' + "request body: " + req.body })
+  res.status(200).json({ message: "" + req.query.message })
   console.log(req.body);
   const filePath = 'phonenumbers.csv';
 //   fs.writeFileSync(filePath, csvContents, 'utf8')
 }
 
+// function to access phonenumbers.csv and add the phone number from the api request to the csv file
