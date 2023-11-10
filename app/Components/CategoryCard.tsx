a338e61f206820b9b871dd5b8518ab9221ec7cf3
Flipfloppm <franklin.s.dai@gmail.com>
2023-11-10 16:51:22 -0600

    Added color transition on hover

--- app/Components/CategoryCard.tsx
+++ app/Components/CategoryCard.tsx
@@ -7,26 +7,23 @@ import {
 } from "@material-tailwind/react";
 
 export type CategoryCardProps = {
-    vendorName: string,
-    image: string
-}
+    vendorName: string;
+    image: string;
+};
 
 export default function CategoryCard(props: CategoryCardProps) {
     return (
-        <Card className="m-6 w-96 border border-black rounded-lg">
+        <Card className="m-5 w-96 border border-black rounded-lg group">
             <CardHeader
                 floated={false}
                 shadow={false}
                 color="transparent"
                 className="m-0 relative h-56 border-b border-black rounded-t-lg"
             >
-                <img
-                    src={props.image}
-                    alt="snoopy"
-                />
+                <img src={props.image} alt="snoopy" />
             </CardHeader>
-            <CardBody className="p-4">
-                <Typography variant="h4" color="blue-gray">
+            <CardBody className="p-4 rounded-b-md group-hover:bg-ballet transition-colors">
+                <Typography className="h4 group-hover:text-white transition-colors">
                     {props.vendorName}
                 </Typography>
             </CardBody>
