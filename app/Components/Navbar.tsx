462d65301700c8f1e8c4c11a2a2855d3f518a908
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2023-11-27 17:28:20 -0600

    Change a to Link

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,36 +1,38 @@
 "use client";
 
+import Link from "next/link";
+
 export default function NavBar() {
     return (
         <nav className="bg-ballet p-2 flex justify-between items-center w-full">
             {/* Logo in the top left corner */}
-            <a href="/" className="shrink-0">
+            <Link href="/" className="shrink-0">
                 <img
                     src="/logo.png" 
                     alt="Company Logo"
                     className="h-10 w-auto"
                 />
-            </a>
+            </Link>
 
             {/* Centered navbar items */}
             <div className="flex-grow">
                 <div className="hidden items-center justify-center md:flex">
                     <ul className="flex justify-center flex-wrap items-center mt-4 md:mt-0">
                         <li>
-                            <a
-                                href="/vendorspage"
+                            <Link
+                                href="/vendors"
                                 className="block py-2 px-4 text-white hover:bg-gray-200 hover:text-gray-700 md:hover:bg-transparent md:hover:text-blue-700"
                             >
                                 Vendors
-                            </a>
+                            </Link>
                         </li>
                         <li>
-                            <a
-                                href="/categoriespage"
+                            <Link
+                                href="/categories"
                                 className="block py-2 px-4 text-white hover:bg-gray-200 hover:text-gray-700 md:hover:bg-transparent md:hover:text-blue-700"
                             >
                                 Categories
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
