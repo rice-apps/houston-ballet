2a8ca2748aa974893eced9e22e0b44226b68e4c2
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-01-26 17:23:51 -0600

    Add map to navigation bar

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -4,23 +4,23 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <nav className="bg-ballet p-2 flex justify-between items-center w-full">
+        <nav className="flex w-full items-center justify-between bg-ballet p-2">
             {/* Logo in the top left corner */}
             <Link href="/" className="shrink-0">
                 <img
-                    src="/logo.png" 
+                    src="/logo.png"
                     alt="Company Logo"
                     className="h-10 w-auto"
                 />
             </Link>
 
             <div className="flex-grow">
-                <div className="hidden items-left justify-left md:flex">
-                    <ul className="flex justify-left flex-wrap items-left mt-4 md:mt-0">
+                <div className="items-left justify-left hidden md:flex">
+                    <ul className="justify-left items-left mt-4 flex flex-wrap md:mt-0">
                         <li>
                             <Link
                                 href="/vendors"
-                                className="block py-2 px-4 text-white hover:bg-gray-200 hover:text-gray-700 md:hover:bg-transparent md:hover:text-blue-700"
+                                className="block px-4 py-2 text-white hover:bg-gray-200 hover:text-gray-700 md:hover:bg-transparent md:hover:text-blue-700"
                             >
                                 Vendors
                             </Link>
@@ -28,9 +28,17 @@ export default function NavBar() {
                         <li>
                             <Link
                                 href="/categories"
-                                className="block py-2 px-4 text-white hover:bg-gray-200 hover:text-gray-700 md:hover:bg-transparent md:hover:text-blue-700"
+                                className="block px-4 py-2 text-white hover:bg-gray-200 hover:text-gray-700 md:hover:bg-transparent md:hover:text-blue-700"
                             >
                                 Categories
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/map"
+                                className="block px-4 py-2 text-white hover:bg-gray-200 hover:text-gray-700 md:hover:bg-transparent md:hover:text-blue-700"
+                            >
+                                Map
                             </Link>
                         </li>
                     </ul>
