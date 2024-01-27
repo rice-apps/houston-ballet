8e1e96db1e56cce29cf3017514462abf189a779a
benfliu <ben.franklin.liu@gmail.com>
2024-01-27 15:55:46 -0600

    carousel working

--- app/Components/VendorImages.tsx
+++ app/Components/VendorImages.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Carousel, Button } from 'flowbite-react';
+import { Carousel } from 'flowbite-react';
 
 export default function VendorImages() {
     return (
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 w-100 bg-black">
-            <Carousel className="h-50 w-54">
+        <div>
+            <Carousel className="h-60 w-60">
                 <div>
                     <img src="/alaska_fur.png" alt="..." />
                 </div>
