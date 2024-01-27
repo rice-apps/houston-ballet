e932250eb0af25a811dd7e246e9c3c6044fcce91
benfliu <ben.franklin.liu@gmail.com>
2024-01-27 17:04:56 -0600

    connected with strapi

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        'node_modules/flowbite-react/lib/esm/**/*.js',
     ],
     theme: {
         fontFamily: {
@@ -48,5 +49,5 @@ module.exports = {
           },
         },
     },
-  plugins: [],
+  plugins: [require('flowbite/plugin'),],
 }
