73983cd524a4623965c9e731cd59c50f596c3f98
benfliu <ben.franklin.liu@gmail.com>
2024-02-02 14:57:14 -0600

    added images again

--- app/Components/VendorImages.tsx
+++ app/Components/VendorImages.tsx
@@ -5,12 +5,6 @@ export default function VendorImages() {
     return (
         <div>
             <Carousel className="h-60 w-60">
-                {/* <div>
-                    <img src="/akfurgallery1.jpg" alt="..." />
-                </div>
-                <div>
-                    <img src="/akfurgallery2.jpeg" alt="..." />
-                </div> */}
                 <div>
                     <img src="/akfurgallery3.png" alt="..." />
                 </div>
