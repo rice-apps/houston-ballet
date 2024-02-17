545e042fcd94a183f1794a7280a069c2be775bc2
Flipfloppm <franklin.s.dai@gmail.com>
2024-02-17 14:14:54 -0600

    fixed blue links and underlining

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -108,7 +108,9 @@ export default function NavBar() {
                             <li key={idx}>
                                 <Link
                                     href={link.path}
-                                    className="ml-5 block px-4 py-2 text-white hover:bg-gray-200 md:hover:bg-transparent md:hover:underline"
+                                    color="#FFFFFF"
+                                    underline="hover"
+                                    className="ml-5 block px-4 py-2 hover:underline"
                                 >
                                     {link.displayText}
                                 </Link>
