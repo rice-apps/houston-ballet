7f680d484208d3ecbbb33cb5ed61686ecdd0e00d
anyaguuu <ag156@rice.edu>
2024-03-02 15:57:14 -0600

    added interests

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -24,22 +24,20 @@ export default async function WelcomePage() {
                 <h1 className='font-sans top-40 bg-none text-center text-4xl font-bold tracking-[.15em] text-white md:text-7xl'>WELCOME!</h1>
                 <h2 className='pt-3 text-center text-lg md:text-xl font-medium tracking-[.15em] text-white'>FIRST TIME AT THE MARKET? </h2>
             </div>
-            <div className="h-full w-1/2 flex items-centerå">
-                <div className="font-sans text-4xl text-[#423E4C]">Choose Your Interests ...</div>
+            <div className="h-full w-1/2 flex flex-col">
+                <div className="font-sans text-4xl text-[#423E4C] w-full">Choose Your Interests ...</div>
+                <div className='absolute z-10 right-0'> */
+                <InterestsButton allCategories={allCategories} />
+                    {tempCategoryArray.map((category, index) => (
+                        <button
+                            className="h-10 px-5 text-gray-600 type = radio transition-colors duration-150 border border-gray-600 rounded-lg focus:shadow-outline hover:bg-gray-600 focus:bg-white hover:text-indigo-100"
+                            key={index}
+                        >
+                            {category.categoryName}
+                        </button>
+                    ))}
+                </div>
             </div>
-        </div>
-
-
-        <div className='absolute z-10 right-0'>ååå */
-            <InterestsButton allCategories={allCategories} />
-            {tempCategoryArray.map((category, index) => (
-                <button
-                    className="h-10 px-5 text-gray-600 type = radio transition-colors duration-150 border border-gray-600 rounded-lg focus:shadow-outline hover:bg-gray-600 focus:bg-white hover:text-indigo-100"
-                    key={index}
-                >
-                    {category.categoryName}
-                </button>
-            ))}
         </div>
         </>
     );
