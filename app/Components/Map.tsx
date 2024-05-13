f07e7b0b02ae0c0cac241c2fb6de00fd9364c994
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-05-13 14:46:42 -0500

    Fix height for embed

--- app/Components/Map.tsx
+++ app/Components/Map.tsx
@@ -7,13 +7,13 @@ export default function Map(){
  
   const location = locationParams?.get('location')
   return (
-    <div className='bg-white flex flex-col content-center justify-center'>
+    <div className='bg-white flex flex-col content-center justify-center h-screen'>
       <iframe 
         title="Mappedin Map" 
         name="Mappedin Map" 
         scrolling="no" 
         width="100%" 
-        height="850" 
+        height="100%" 
         src={`https://maker.mappedin.com/map/65347b743538e4288124470d?embedded=true%22%3E&map=Ground%20Floor&location=${location}`}
         ></iframe>
     </div>
