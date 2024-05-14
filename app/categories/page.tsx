f8870fe1d78e92c0463a32f61b6a97d857272761
Ahmed A <95593064+AhmedA1559@users.noreply.github.com>
2024-05-14 01:27:40 -0500

    Add subtitles (#30)
    Co-authored-by: Stephen Xu <smx2@rice.edu>

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -4,6 +4,7 @@ import CategoryCard, { CategoryCardProps } from "../Components/CategoryCard";
 export default async function CategoryPage() {
     // temp card array to be replaced by database info
     const categories = await getCategories();
+    const subtitle = (await getCategories()).getSubtitle("categories");
 
     const tempCardArray: Array<CategoryCardProps> = [];
     for (let category of categories.getCategories()) {
@@ -21,8 +22,7 @@ export default async function CategoryPage() {
                     CATEGORIES
                 </h1>
                 <h3 className="pt-3 text-center text-lg md:text-xl font-medium tracking-[.15em] text-white">
-                    BROWSE THROUGH OUR VENDORS WITH OUR DIVERSE LIST OF
-                    CATEGORIES.
+                    {subtitle ?? ""}
                 </h3>
             </header>
             <div className="py-6 px-5 sm:px-5 md:px-10 lg:px-20">
