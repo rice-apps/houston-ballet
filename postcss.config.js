c8a437d2879846f058c6fa6abb54bf260e418baf
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-05-15 18:10:54 -0500

    Revert "Revert "Add Purge CSS""
    This reverts commit 6aebd1f10520b1e832c83ea7c003b100aeb2e9f1.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: [
+      './app/**/*.{js,jsx,ts,tsx}',
+      './pages/**/*.{js,jsx,ts,tsx}',
+      './components/**/*.{js,jsx,ts,tsx}',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
-}
+];
+
+module.exports = {
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
+};
