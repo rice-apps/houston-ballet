61a84ab4a68edf867eaff400cfb891866ab3e5e7
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-05-15 18:18:28 -0500

    Add lazy script loading

--- app/Components/GoogleAnalytics.tsx
+++ app/Components/GoogleAnalytics.tsx
@@ -6,9 +6,9 @@ const GoogleAnalytics = ({ GA_TRACKING_ID }: { GA_TRACKING_ID: string }) => {
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        strategy="afterInteractive"
+        strategy="worker"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-analytics" strategy="worker">
         {`
         window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
