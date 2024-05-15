3e85bbd2f12d4c55a5bbde10b8fce99fcdba32c5
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-05-15 18:48:21 -0500

    Add bundle analysis

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
     // output: "export",
     images: {
@@ -19,4 +23,4 @@ const nextConfig = {
     },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
