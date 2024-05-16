2fb4e1246276eaea5dd8c844d6c476b82268c323
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-05-15 19:34:26 -0500

    Fix compile error

--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -35,10 +35,10 @@ export async function getCategories(): VendorsRepo {
     categoriesResp.data.forEach(category => {
       if (process.env.S3_URL && process.env.CFRONT_URL) {
         if (category?.primaryImage?.url) {
-          category?.primaryImage?.url = category?.primaryImage?.url.replace(process.env.S3_URL, process.env.CFRONT_URL)
+          category?.primaryImage?.url.replace(process.env.S3_URL, process.env.CFRONT_URL)
         }
         if (category?.smallIcon?.url) {
-          category?.smallIcon?.url = category?.smallIcon?.url.replace(process.env.S3_URL, process.env.CFRONT_URL)
+          category?.smallIcon?.url.replace(process.env.S3_URL, process.env.CFRONT_URL)
         }
       }
       ret[category.name] = {vendors: [], image: category?.primaryImage?.url ?? "", id: category.id, smallIcon: category?.smallIcon?.url ?? ""}
@@ -48,14 +48,14 @@ export async function getCategories(): VendorsRepo {
       let categories = vendor.categories;
       categories.forEach(category => {
         let vendor_desc = {};
-        
+
         if (process.env.S3_URL && process.env.CFRONT_URL) {
           if (vendor?.primaryImage?.url) {
-            vendor?.primaryImage?.url = vendor?.primaryImage?.url.replace(process.env.S3_URL, process.env.CFRONT_URL)
+            vendor?.primaryImage?.url.replace(process.env.S3_URL, process.env.CFRONT_URL)
           }
           if (vendor?.additionalImages) {
             vendor.additionalImages.forEach(image => {
-              image.url = image.url.replace(process.env.S3_URL, process.env.CFRONT_URL)
+              image.url.replace(process.env.S3_URL, process.env.CFRONT_URL)
             });
           }
         }
