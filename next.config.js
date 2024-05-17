949043198a17f4801110131ffd4a99ad27f92cf6
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2024-05-17 12:20:01 -0500

    Set next config to output

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 const nextConfig = {
-    // output: "export",
+    output: "export",
     images: {
         remotePatterns: [
             {
