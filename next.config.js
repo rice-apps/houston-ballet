6f9681d70bbe14c0db9a6d963f7b3f426a7a5327
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-01-31 12:19:39 -0600

    Add new bucket name

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
+                hostname: 'hb-nutcracker-webapp.s3.us-east-1.amazonaws.com',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
                 hostname: 'd13lc4w4003kwe.cloudfront.net',
                 pathname: '**',
             },
